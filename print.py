2a24963d735c881243fd1960c772fd115c2f9c6d
akhalitov <akhalitov@ecombix>
2018-11-13 12:08:48 +0300

    1

---
+++ print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+one = 'once'
+too = 'too'
+three = 3
+
+print("{0},{1},{2} and {0}".format(one, too, three))
+print("{0},{1},{2} and {0}".format(one, too, three))
+print('{0:H^9}'.format('main'))
+
+
